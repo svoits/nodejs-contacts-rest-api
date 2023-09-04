6b5b45c82dcfd517f5d88e83f0c3ca5eb186203f
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-09-04 20:20:38 +0200

    solved

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,12 +4,16 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "email");
+  const result = await Contact.find(
+    favorite ? { owner, favorite } : { owner },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "email");
   res.json(result);
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const controller = require("../controllers/auth");
-const { validateBody, authenticate } = require("../middlewares");
+const { validateBody, authenticate, upload } = require("../middlewares");
 const { schemas } = require("../models/user");
 
 const router = express.Router();
@@ -16,5 +16,19 @@ router.post("/login", validateBody(schemas.loginSchema), controller.login);
 router.get("/current", authenticate, controller.getCurrent);
 
 router.post("/logout", authenticate, controller.logout);
+
+router.patch(
+  "/",
+  authenticate,
+  validateBody(schemas.updateSubscriptionSchema),
+  controller.updateSubscription
+);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  controller.updateAvatar
+);
 
 module.exports = router;
